701ba43075a416580d9723f3371b7583a4bb0f32
megler <majeresmom@att.net>
2022-04-23 12:43:00 -0400

    removed contact form functionality

--- restful_blog/home/routes.py
+++ restful_blog/home/routes.py
@@ -27,34 +27,34 @@ def about():
 
 @home_bp.route("/contact", methods=["GET", "POST"])
 def contact():
-    api_key = app.config["API_KEY"]
-    api_secret = app.config["API_SECRET"]
-    """Contact Page"""
-    if request.method == "POST":
-        sname = request.form["name"]
-        semail = request.form["email"]
-        smessage = request.form["message"]
-        mailjet = Client(auth=(api_key, api_secret), version="v3.1")
-        data = {
-            "Messages": [{
-                "From": {
-                    "Email": os.environ["MJ_SENDER_EMAIL"],
-                    "Name": "Marceia with Python Study Group",
-                },
-                "To": [{
-                    "Email": os.environ["MJ_RECEIVER_EMAIL"],
-                    "Name": "Marceia"
-                }],
-                "Subject":
-                "Contact Form From Your blog",
-                "TextPart":
-                f"From: {sname}\nEmail: {semail}\nMessage: {smessage}",
-            }]
-        }
-        result = mailjet.send.create(data=data)
-        print(result.status_code)
-        print(result.json())
-        if result.status_code == 200:
-            flash("Your email was sent. Someone will get back with you soon.")
-            return redirect(url_for("home_bp.get_all_posts"))
+    # api_key = app.config["API_KEY"]
+    # api_secret = app.config["API_SECRET"]
+    # """Contact Page"""
+    # if request.method == "POST":
+    #     sname = request.form["name"]
+    #     semail = request.form["email"]
+    #     smessage = request.form["message"]
+    #     mailjet = Client(auth=(api_key, api_secret), version="v3.1")
+    #     data = {
+    #         "Messages": [{
+    #             "From": {
+    #                 "Email": os.environ["MJ_SENDER_EMAIL"],
+    #                 "Name": "Marceia with Python Study Group",
+    #             },
+    #             "To": [{
+    #                 "Email": os.environ["MJ_RECEIVER_EMAIL"],
+    #                 "Name": "Marceia"
+    #             }],
+    #             "Subject":
+    #             "Contact Form From Your blog",
+    #             "TextPart":
+    #             f"From: {sname}\nEmail: {semail}\nMessage: {smessage}",
+    #         }]
+    #     }
+    #     result = mailjet.send.create(data=data)
+    #     print(result.status_code)
+    #     print(result.json())
+    #     if result.status_code == 200:
+    #         flash("Your email was sent. Someone will get back with you soon.")
+    #         return redirect(url_for("home_bp.get_all_posts"))
     return render_template("contact.html")
